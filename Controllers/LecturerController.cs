4992043e30ee8bbd78a220b24341cda779fad433
phanhoduyphuong <phanhoduyphuong@gmail.com>
2023-03-11 14:34:25 +0700

    initial project

---
+++ Controllers/LecturerController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoMvc.Controllers
+{
+    public class LecturerController : Controller
+    {
+        // GET: Lucturer
+        public ActionResult Index()
+        {
+            ViewBag.Id = "A001";
+            ViewBag.Name = "Nguyen Van A";
+            ViewData["Add"] = "TpHCM";
+            return View();
+        }
+        public ActionResult Hello()
+        {
+            return Content("Hello everyone!!!");
+        }
+        public ActionResult GoToGG()
+        {
+            return Redirect("https://google.com");
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace DemoMvc.Models
+{
+    public class Student
+    {
+        [DisplayName("Mã sinh viên")]
+        public string Id { get; set; }
+        [DisplayName("Tên sinh viên")]
+        public string Name { get; set; }
+        [DisplayName("Địa chỉ")]
+        public string Address { get; set; }
+        [DisplayName("Giới tính")]
+        public bool Gender { get; set; }
+        [DisplayName("Điểm")]
+        public float Avg { get; set; }
+    }
+}
